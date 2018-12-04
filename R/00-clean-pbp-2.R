a9e8bd8fced4080ae86c7994b1ced5cea8909d5f
Tony ElHabr <anthonyelhabr@gmail.com>
2018-12-04 11:29:46 -0600

    working on cleaning v2

--- R/00-clean-pbp-2.R
+++ R/00-clean-pbp-2.R
@@ -1,6 +1,12 @@
 
 # helfRlein::clean_gc()
 # https://drive.google.com/drive/folders/1GMiP-3Aoh2AKFCoGZ8f0teMYNlkm87dm
+tempfile <- tempfile(fileext = ".zip")
+dir_game_summary_raw <-
+  "https://drive.google.com/drive/folders/1GMiP-3Aoh2AKFCoGZ8f0teMYNlkm87dm&export=download" %>%
+  # archive::archive_read()
+  download.file(destfile = tempfile)
+.tempfile_unzipped <- utils::unzip(.tempfile, exdir = tempdir())
 
 # Check that it doesn't already exist when re-sourcing this script in order
 # to reduce run-time.
